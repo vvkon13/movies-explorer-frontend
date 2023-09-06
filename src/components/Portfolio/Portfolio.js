eb808f5d35fd77eecbc870caa5950a801449feb6
Vasiliy Konovalov <vvkon13@gmail.com>
2023-09-06 10:58:58 +0400

    Пункт1: исправлены ссылки

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,36 +4,54 @@ import "./Portfolio.css";
 function Portfolio() {
     return (
         <section className="portfolio app__portfolio">
-            <h1 className="portfolio__title">
+            <a href="https://github.com/" target="_blank" className="portfolio__title app__link" rel="noopener noreferrer">
                 Github
-            </h1>
+            </a>
             <h2 className="portfolio__subtitle">
                 Портфолио
             </h2>
             <nav>
                 <ul className="portfolio__table">
-                    <li className="portfolio__string-link portfolio__string-link_underlined">
-                        <a href="https://github.com/" target="_blank" className="portfolio__link app__link" rel="noopener noreferrer">
-                            Статичный сайт
-                        </a>
-                        <a href="https://github.com/" target="_blank" className="portfolio__link-icon app__link" rel="noopener noreferrer">
-                            ↗
-                        </a>
-                    </li>
-                    <li className="portfolio__string-link portfolio__string-link_underlined">
-                        <a href="https://github.com/" target="_blank" className="portfolio__link app__link" rel="noopener noreferrer">
-                            Адаптивный сайт
-                        </a>
-                        <a href="https://github.com/" target="_blank" className="portfolio__link-icon app__link" rel="noopener noreferrer">
-                            ↗
+                    <li>
+                        <a
+                            href="https://github.com/vvkon13/how-to-learn.git"
+                            target="_blank"
+                            className="portfolio__link portfolio__link_underlined app__link"
+                            rel="noopener noreferrer">
+                            <p className="portfolio__link-text">
+                                Статичный сайт
+                            </p>
+                            <p className="portfolio__link-icon">
+                                ↗
+                            </p>
                         </a>
                     </li>
-                    <li className="portfolio__string-link">
-                        <a href="https://github.com/" target="_blank" className="portfolio__link app__link" rel="noopener noreferrer">
-                            Одностраничное приложение
+                    <li>
+                        <a
+                            href="https://github.com/vvkon13/russian-travel.git/"
+                            target="_blank"
+                            className="portfolio__link portfolio__link_underlined app__link"
+                            rel="noopener noreferrer">
+                            <p className="portfolio__link-text">
+                                Адаптивный сайт
+                            </p>
+                            <p className="portfolio__link-icon">
+                                ↗
+                            </p>
                         </a>
-                        <a href="https://github.com/" target="_blank" className="portfolio__link-icon app__link" rel="noopener noreferrer">
-                            ↗
+                    </li>
+                    <li>
+                        <a
+                            href="https://github.com/vvkon13/react-mesto-api-full-gha.git"
+                            target="_blank"
+                            className="portfolio__link app__link"
+                            rel="noopener noreferrer">
+                            <p className="portfolio__link-text">
+                                Одностраничное приложение
+                            </p>
+                            <p className="portfolio__link-icon">
+                                ↗
+                            </p>
                         </a>
                     </li>
                 </ul>
